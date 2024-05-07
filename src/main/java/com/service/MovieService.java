ff9416033c5828c26edc61ccb36324f0b7fcbf51
Tohap <t.pasaribu@eksad.com>
2024-05-08 04:14:48 +0700

    adding try catch

--- src/main/java/com/service/MovieService.java
+++ src/main/java/com/service/MovieService.java
@@ -47,66 +47,77 @@ public class MovieService {
         if (dto.getId() == null || dto.getId() == 0 || dto.getId().equals("")){
             return new ResponseEntity("format id salah",HttpStatus.BAD_REQUEST);
         }
+        
+        try{
+            MovieModel data = repository.findAllById(dto.getId());
 
-        MovieModel data = repository.findAllById(dto.getId());
+            if (data != null){
+                data.setTitle(dto.getTitle());
+                data.setDescription(dto.getDescription());
+                data.setRating(dto.getRating());
+                data.setImage(dto.getImage());
+                data.setCreated_at(dto.getCreated_at());
+                data.setUpdated_at(dto.getUpdated_at());
+                repository.save(data);
 
-        if (data != null){
-            data.setTitle(dto.getTitle());
-            data.setDescription(dto.getDescription());
-            data.setRating(dto.getRating());
-            data.setImage(dto.getImage());
-            data.setCreated_at(dto.getCreated_at());
-            data.setUpdated_at(dto.getUpdated_at());
-            repository.save(data);
+            }else{
+                MovieModel newData = new MovieModel();
+                newData.setId(dto.getId());
+                newData.setTitle(dto.getTitle());
+                newData.setDescription(dto.getDescription());
+                newData.setRating(dto.getRating());
+                newData.setImage(dto.getImage());
+                newData.setCreated_at(dto.getCreated_at());
+                newData.setUpdated_at(dto.getUpdated_at());
+                repository.save(newData);
+            }
 
-        }else{
-            MovieModel newData = new MovieModel();
-            newData.setId(dto.getId());
-            newData.setTitle(dto.getTitle());
-            newData.setDescription(dto.getDescription());
-            newData.setRating(dto.getRating());
-            newData.setImage(dto.getImage());
-            newData.setCreated_at(dto.getCreated_at());
-            newData.setUpdated_at(dto.getUpdated_at());
-            repository.save(newData);
+            return new ResponseEntity(dto,HttpStatus.OK);
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
-
-        return new ResponseEntity(dto,HttpStatus.OK);
-
     }
 
     public ResponseEntity<List<MovieRequestDTO>>updateMovie(MovieRequestDTO dto){
         if (dto.getId() == null || dto.getId() == 0 || dto.getId().equals("")){
             return new ResponseEntity("format id salah",HttpStatus.BAD_REQUEST);
         }
+        
+        try{
+            MovieModel data = repository.findAllById(dto.getId());
 
-        MovieModel data = repository.findAllById(dto.getId());
+            if (data != null){
+                data.setTitle(dto.getTitle());
+                data.setDescription(dto.getDescription());
+                data.setRating(dto.getRating());
+                data.setImage(dto.getImage());
+                data.setCreated_at(dto.getCreated_at());
+                data.setUpdated_at(dto.getUpdated_at());
+                repository.save(data);
 
-        if (data != null){
-            data.setTitle(dto.getTitle());
-            data.setDescription(dto.getDescription());
-            data.setRating(dto.getRating());
-            data.setImage(dto.getImage());
-            data.setCreated_at(dto.getCreated_at());
-            data.setUpdated_at(dto.getUpdated_at());
-            repository.save(data);
+                return new ResponseEntity(dto,HttpStatus.OK);
 
-            return new ResponseEntity(dto,HttpStatus.OK);
-
-        }else{
-            return new ResponseEntity("Id tidak ditemukan",HttpStatus.OK);
+            }else{
+                return new ResponseEntity("Id tidak ditemukan",HttpStatus.OK);
+            }
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
     }
 
     public ResponseEntity<MovieModel>deleteById(Integer id){
-        MovieModel data = repository.findAllById(id);
+        try{
+            MovieModel data = repository.findAllById(id);
 
-        if (data != null){
-            repository.delete(data);
+            if (data != null){
+                repository.delete(data);
 
-            return new ResponseEntity(HttpStatus.OK);
-        }else{
-            return new ResponseEntity("Id tidak ditemukan",HttpStatus.OK);
+                return new ResponseEntity(HttpStatus.OK);
+            }else{
+                return new ResponseEntity("Id tidak ditemukan",HttpStatus.OK);
+            }
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
     }
 }
